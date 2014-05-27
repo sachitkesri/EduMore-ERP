132869ce80d8a5690aae396b91cdca436cf3b272
sachitkesri <sachitkesri@gmail.com>
2014-05-27 18:01:55 +0530

    SQL COmmit

--- EduMore--ERP/edumore.sql
+++ EduMore--ERP/edumore.sql
@@ -1,8 +1,9 @@
 /*
-SQLyog Community v10.0 Beta2
-MySQL - 5.0.20-nt : Database - edumore
+SQLyog Community v11.5 (64 bit)
+MySQL - 5.5.30 : Database - edumore
 *********************************************************************
 */
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -21,20 +22,20 @@ USE `edumore`;
 DROP TABLE IF EXISTS `address_details`;
 
 CREATE TABLE `address_details` (
-  `address_id` bigint(20) unsigned NOT NULL auto_increment,
-  `house_number` varchar(30) default NULL,
-  `building_name` varchar(30) default NULL,
-  `street` varchar(60) default NULL,
-  `area` varchar(90) default NULL,
-  `city` varchar(30) default NULL,
-  `state` varchar(30) default NULL,
-  `country` varchar(30) default 'India',
-  `pincode` varchar(20) default NULL,
+  `address_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `house_number` varchar(30) DEFAULT NULL,
+  `building_name` varchar(30) DEFAULT NULL,
+  `street` varchar(60) DEFAULT NULL,
+  `area` varchar(90) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `state` varchar(30) DEFAULT NULL,
+  `country` varchar(30) DEFAULT 'India',
+  `pincode` varchar(20) DEFAULT NULL,
   `enquiry_number` bigint(20) unsigned NOT NULL,
-  `address_type` varchar(10) default NULL,
-  PRIMARY KEY  (`address_id`),
+  `address_type` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`address_id`),
   KEY `enquiry_id` (`enquiry_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
 
 /*Data for the table `address_details` */
 
@@ -45,11 +46,11 @@ insert  into `address_details`(`address_id`,`house_number`,`building_name`,`stre
 DROP TABLE IF EXISTS `admin_login_details`;
 
 CREATE TABLE `admin_login_details` (
-  `admin_id` bigint(20) unsigned NOT NULL auto_increment,
-  `admin_username` varchar(20) default NULL,
-  `admin_password` varchar(20) default NULL,
-  PRIMARY KEY  (`admin_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `admin_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `admin_username` varchar(20) DEFAULT NULL,
+  `admin_password` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 /*Data for the table `admin_login_details` */
 
@@ -60,11 +61,11 @@ insert  into `admin_login_details`(`admin_id`,`admin_username`,`admin_password`)
 DROP TABLE IF EXISTS `counsellor_login_details`;
 
 CREATE TABLE `counsellor_login_details` (
-  `counsellor_id` bigint(20) unsigned NOT NULL auto_increment,
-  `counsellor_username` varchar(20) default NULL,
-  `counsellor_password` varchar(20) default NULL,
-  PRIMARY KEY  (`counsellor_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `counsellor_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `counsellor_username` varchar(20) DEFAULT NULL,
+  `counsellor_password` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`counsellor_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 /*Data for the table `counsellor_login_details` */
 
@@ -75,14 +76,14 @@ insert  into `counsellor_login_details`(`counsellor_id`,`counsellor_username`,`c
 DROP TABLE IF EXISTS `course_details`;
 
 CREATE TABLE `course_details` (
-  `course_id` bigint(20) unsigned NOT NULL auto_increment,
-  `course_name` varchar(60) default NULL,
-  `course_duration` int(10) unsigned default NULL,
-  `admin_id` bigint(20) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`course_id`),
+  `course_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(60) DEFAULT NULL,
+  `course_duration` int(10) unsigned DEFAULT NULL,
+  `admin_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`course_id`),
   KEY `FK_course_details` (`admin_id`),
   CONSTRAINT `FK_course_details` FOREIGN KEY (`admin_id`) REFERENCES `admin_login_details` (`admin_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 /*Data for the table `course_details` */
 
@@ -93,53 +94,55 @@ insert  into `course_details`(`course_id`,`course_name`,`course_duration`,`admin
 DROP TABLE IF EXISTS `enquiry_details`;
 
 CREATE TABLE `enquiry_details` (
-  `enquiry_id` bigint(20) unsigned NOT NULL auto_increment,
+  `enquiry_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) NOT NULL,
-  `middle_name` varchar(50) default NULL,
-  `last_name` varchar(50) default NULL,
-  `residence_number` varchar(20) default NULL,
-  `mobile_number` varchar(20) default NULL,
-  `email_id` varchar(50) default NULL,
-  `date_of_birth` varchar(50) default NULL,
-  `gender` varchar(6) default NULL,
-  `father_first_name` varchar(25) default NULL,
-  `father_middle_name` varchar(50) default NULL,
-  `father_last_name` varchar(50) default NULL,
-  `father_mobile_number` varchar(20) default NULL,
-  `father_residence_number` varchar(20) default NULL,
-  `current_address` bigint(20) unsigned zerofill default NULL,
-  `permanent_address` bigint(20) unsigned zerofill default NULL,
+  `middle_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `residence_number` varchar(20) DEFAULT NULL,
+  `mobile_number` varchar(20) DEFAULT NULL,
+  `email_id` varchar(50) DEFAULT NULL,
+  `date_of_birth` varchar(50) DEFAULT NULL,
+  `gender` varchar(6) DEFAULT NULL,
+  `father_first_name` varchar(25) DEFAULT NULL,
+  `father_middle_name` varchar(50) DEFAULT NULL,
+  `father_last_name` varchar(50) DEFAULT NULL,
+  `father_mobile_number` varchar(20) DEFAULT NULL,
+  `father_residence_number` varchar(20) DEFAULT NULL,
+  `current_address` bigint(20) unsigned zerofill DEFAULT NULL,
+  `permanent_address` bigint(20) unsigned zerofill DEFAULT NULL,
   `counsellor_id` bigint(20) unsigned NOT NULL,
   `enquiry_number` bigint(20) unsigned NOT NULL,
-  PRIMARY KEY  (`enquiry_id`),
+  `enquiry_status` varchar(20) DEFAULT 'open',
+  `admission_status` varchar(20) NOT NULL DEFAULT 'hold',
+  PRIMARY KEY (`enquiry_id`),
   KEY `FK_enquiry_details` (`counsellor_id`),
   KEY `enquiry_details_ibfk_2` (`current_address`),
   KEY `enquiry_id` (`enquiry_id`),
   CONSTRAINT `enquiry_details_ibfk_1` FOREIGN KEY (`counsellor_id`) REFERENCES `counsellor_login_details` (`counsellor_id`),
   CONSTRAINT `enquiry_details_ibfk_2` FOREIGN KEY (`current_address`) REFERENCES `address_details` (`address_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 /*Data for the table `enquiry_details` */
 
-insert  into `enquiry_details`(`enquiry_id`,`first_name`,`middle_name`,`last_name`,`residence_number`,`mobile_number`,`email_id`,`date_of_birth`,`gender`,`father_first_name`,`father_middle_name`,`father_last_name`,`father_mobile_number`,`father_residence_number`,`current_address`,`permanent_address`,`counsellor_id`,`enquiry_number`) values (1,'Sachit Kesri',NULL,NULL,'9971840843',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,00000000000000000000,1,0),(2,'ssss','','','','',NULL,'','','','','null','null','null',00000000000000000031,00000000000000000032,1,1399342441182),(3,'asd','hgj','gjh','gjh','gjh',NULL,'g','jh','g','jhg','jh','g','ghj',00000000000000000033,00000000000000000034,1,1399342483749),(4,'','','','','',NULL,'','','','','','','',00000000000000000035,00000000000000000036,1,1400597715049);
+insert  into `enquiry_details`(`enquiry_id`,`first_name`,`middle_name`,`last_name`,`residence_number`,`mobile_number`,`email_id`,`date_of_birth`,`gender`,`father_first_name`,`father_middle_name`,`father_last_name`,`father_mobile_number`,`father_residence_number`,`current_address`,`permanent_address`,`counsellor_id`,`enquiry_number`,`enquiry_status`,`admission_status`) values (1,'Sachit Kesri',NULL,NULL,'9971840843',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,00000000000000000000,1,0,'close','notinterested'),(2,'ssss','','','','',NULL,'','','','','null','null','null',00000000000000000031,00000000000000000032,1,1399342441182,'open','hold'),(3,'asd','hgj','gjh','gjh','gjh',NULL,'g','jh','g','jhg','jh','g','ghj',00000000000000000033,00000000000000000034,1,1399342483749,'open','hold'),(4,'','','','','',NULL,'','','','','','','',00000000000000000035,00000000000000000036,1,1400597715049,'open','hold');
 
 /*Table structure for table `followup_details` */
 
 DROP TABLE IF EXISTS `followup_details`;
 
 CREATE TABLE `followup_details` (
-  `followup_id` bigint(20) unsigned NOT NULL auto_increment,
-  `date` date default NULL,
-  `remarks` varchar(100) default NULL,
+  `followup_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `date` date DEFAULT NULL,
+  `remarks` varchar(100) DEFAULT NULL,
   `enquiry_id` bigint(20) unsigned NOT NULL,
-  PRIMARY KEY  (`followup_id`),
+  PRIMARY KEY (`followup_id`),
   KEY `enquiry_id` (`enquiry_id`),
   CONSTRAINT `followup_details_ibfk_1` FOREIGN KEY (`enquiry_id`) REFERENCES `enquiry_details` (`enquiry_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 /*Data for the table `followup_details` */
 
-insert  into `followup_details`(`followup_id`,`date`,`remarks`,`enquiry_id`) values (1,'2013-12-09','test remark',1),(2,'2013-12-09','sda',1);
+insert  into `followup_details`(`followup_id`,`date`,`remarks`,`enquiry_id`) values (1,'2013-12-09','test remark',1),(2,'2013-12-09','sda',1),(3,'2013-12-06','Close this',1),(4,'2013-12-06','Close this 234',1),(5,'2013-12-06','Close this 23478',1),(6,'2013-12-06','Close this enquiry',1),(7,'2013-12-06','Close this enquiry again',1),(8,'2013-12-06','Close this enquiry yyy',1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
