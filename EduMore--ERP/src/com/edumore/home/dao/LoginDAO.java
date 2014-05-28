f0055892851d6f15fb2c0bed91a38846d953f153
Sachit Kesri <sachitkesri@gmail.com>
2014-05-28 19:08:56 +0530

    New Files Committed

---
+++ EduMore--ERP/src/com/edumore/home/dao/LoginDAO.java
@@ -0,0 +1,27 @@
+package com.edumore.home.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.edumore.databseconnection.EdumoreDBConnection;
+
+public class LoginDAO {
+	
+	private static Connection con;
+
+	public static int verifyLogin(String username, String password, String loginType) throws SQLException, ClassNotFoundException {
+		con = (Connection) EdumoreDBConnection.getDBConnection();
+		String query = "SELECT * FROM user_login_details where username='"+username+"' AND password='"+password+"' AND role='"+ loginType +"'";  
+		Statement st = null;
+		int userLoginId = 0; 
+		st = con.createStatement();
+		ResultSet rs =  st.executeQuery(query);
+		while(rs.next()){
+			userLoginId = rs.getInt(1);
+		}
+		return userLoginId;
+	}
+
+}
